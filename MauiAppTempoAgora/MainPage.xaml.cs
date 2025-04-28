081f63d4cf54fd09d04c2795a5a77fb48aebbcaa
231283 <231283@CPD02-M06>
2025-04-28 10:51:13 -0300

    clicked Previsao

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora
+﻿using System.Diagnostics;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,17 +12,58 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nacer do sol: {t.sunrise} \n" +
+                                         $"Por do sol: {t.sunset} \n" +
+                                         $"Temp max: {t.temp_max} \n" +
+                                         $"Temp min: {t.temp_min} \n";
+                        lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                            $"type=map&Location=coordinates&metricRain=mm&metricTemp=°C" +
+                            $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                            $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dadoss de Precisão";
+                    }
+
+                }
+                else
+                {
+                    lbl_Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                await DisplayAlert("Ops", ex.Message, "ok");
+            
+            }
+
         }
+
+
+
     }
 
 }
